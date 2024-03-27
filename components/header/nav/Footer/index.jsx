4f5e462696411923ef112c27a9028bae16464b80
Mudit Kapoor <muditk441@gmail.com>
2024-03-27 13:46:17 +0530

    created navbar for now

---
+++ components/header/nav/Footer/index.jsx
@@ -0,0 +1,13 @@
+"use client";
+import styles from "./style.module.scss";
+
+export default function Index() {
+  return (
+    <div className={styles.footer}>
+      <a>Awwwards</a>
+      <a>Instagram</a>
+      <a>Dribble</a>
+      <a>LinkedIn</a>
+    </div>
+  );
+}
