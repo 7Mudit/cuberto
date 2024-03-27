afd0dcac008ad421548c0ea331231dd14436d208
Mudit Kapoor <muditk441@gmail.com>
2024-03-27 16:54:26 +0530

    added staggered animation using gsap to home page banner section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,39 @@
 "use client";
 import styles from "./page.module.scss";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StickyCursor from "@/components/stickyCursor";
 import Navbar from "@/components/shared/Navbar";
 import Banner from "@/components/home/Banner";
+import gsap from "gsap";
 
 export default function Home() {
   const stickyElement = useRef(null);
   const [isActive, setIsActive] = useState(false);
+
+  // for animation purpose
+  const bannerRef = useRef<HTMLDivElement | null>(null); // Reference to the Banner component for animation
+  useEffect(() => {
+    // Ensure elements are present
+    if (bannerRef.current) {
+      const animatableElements = bannerRef.current.querySelectorAll(
+        ".animatable-element"
+      );
+      // GSAP animation for fade in and pop in effect
+      // autoalpha - Animate both CSS opacity and visibility to hide/show elements
+      gsap.set(animatableElements, { autoAlpha: 0, scale: 0.95 });
+
+      // Animate to final state
+      // first param is my target element
+      // second are properties going from intial to final state
+      gsap.to(animatableElements, {
+        duration: 3, // Animation duration in seconds
+        autoAlpha: 1, // Animate to fully visible
+        scale: 1, // Animate to normal size
+        stagger: 0.3,
+        ease: "back.out(1.7)", // Easing for "pop" effect
+      });
+    }
+  }, []); // Empty dependency array means this effect runs once on mount
   return (
     <>
       <Navbar
@@ -21,7 +47,9 @@ export default function Home() {
       <main className={`${styles.main}`}>
         <div className={` ${isActive ? "blurEffect" : ""}`}>
           {/* add your components here an you can change main class */}
-          <Banner />
+          <div ref={bannerRef}>
+            <Banner />
+          </div>
         </div>
       </main>
     </>
--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -15,17 +15,19 @@ const Banner = () => {
     <div className="flex flex-col pl-[20px] sm:pl-[134px] w-full items-start justify-center gap-5">
       <div className="w-full">
         <span
-          className={`text-[46px] leading-[41px] xs:text-[62px] xs:leading-[55px] xl:text-[125px] xl:leading-[112px] ${martel.className}`}
+          className={`text-[46px] leading-[41px] xs:text-[62px] xs:leading-[55px] xl:text-[125px] animatable-element xl:leading-[112px] ${martel.className}`}
         >
           We{" "}
-          <span className={` ${robot.className} !text-[400] !italic`}>
+          <span
+            className={` ${robot.className} animatable-element !text-[400] !italic`}
+          >
             develop
           </span>
         </span>
       </div>
 
       <div className="flex flex-row items-center justify-center gap-2">
-        <div className="w-[160px] h-[110px] overflow-hidden">
+        <div className="w-[160px] animatable-element h-[110px] overflow-hidden">
           <video
             className="pointer-events-none w-[160px] h-[110px] rounded-[40%] object-cover "
             autoPlay
@@ -37,14 +39,14 @@ const Banner = () => {
           </video>
         </div>
         <div
-          className={`text-[24px] leading-[41px] xs:text-[62px] xs:leading-[55px] xl:text-[125px] xl:leading-[112px] ${martel.className}`}
+          className={`text-[24px] leading-[41px] xs:text-[62px] xs:leading-[55px] xl:text-[125px] animatable-element xl:leading-[112px] ${martel.className}`}
         >
           digital products{" "}
         </div>
       </div>
       <div className="w-full">
         <span
-          className={`text-[46px] leading-[41px] xs:text-[62px] xs:leading-[55px] xl:text-[125px] xl:leading-[112px] ${martel.className}`}
+          className={`text-[46px] leading-[41px] xs:text-[62px] xs:leading-[55px] xl:text-[125px] animatable-element xl:leading-[112px] ${martel.className}`}
         >
           from scratch
         </span>
