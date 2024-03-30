5754cbcf816988a0df83a9cfc1ba78cbca1068e4
Mudit Kapoor <muditk441@gmail.com>
2024-03-30 11:19:34 +0530

    logo updated

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -3,16 +3,13 @@ import { Coming_Soon } from "next/font/google";
 const coming = Coming_Soon({ subsets: ["latin"], weight: "400" });
 import { forwardRef } from "react";
 import HeaderMagnetic from "../headerMagnetic/HeaderMagnetic";
+import Image from "next/image";
 
 const Navbar = forwardRef(function Index(props, ref) {
   const { isActive, setIsActive } = props;
   return (
-    <div className="flex top-0 w-screen  min-h-[65px] px-2 sm:px-12 flex-row bg-white justify-between items-center">
-      <h1
-        className={`${coming.className} tracking-normal font-extrabold text-[26px] leading-normal`}
-      >
-        cuberto
-      </h1>
+    <div className="flex top-0 w-screen  min-h-[65px] py-5 px-2 sm:px-12 flex-row bg-white justify-between items-center">
+      <Image src="/assets/logo.png" alt="logo image" width={200} height={200} />
       <HeaderMagnetic ref={ref} isActive={isActive} setIsActive={setIsActive} />
     </div>
   );
