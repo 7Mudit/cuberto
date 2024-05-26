81c3e4a02674a4cb85ea40140d16e7d94277ce1b
Mudit Kapoor <muditk441@gmail.com>
2024-05-26 18:01:40 +0530

    cross button fixed

--- components/headerMagnetic/HeaderMagnetic.jsx
+++ components/headerMagnetic/HeaderMagnetic.jsx
@@ -1,33 +1,36 @@
 import { forwardRef } from "react";
 import styles from "./style.module.scss";
 import Magnetic from "../magnetic";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Nav from "./nav";
 import { AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
+import { FaBars, FaTimes } from "react-icons/fa"; // Import icons
 
 const HeaderMagnetic = forwardRef(function Index(props, ref) {
   const { isActive, setIsActive } = props;
-  // const [isActive, setIsActive] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
     if (isActive) setIsActive(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
+
   return (
     <>
-      {" "}
       <div className={styles.header}>
         <Magnetic>
           <div
             onClick={() => {
               setIsActive(!isActive);
             }}
-            className={`${styles.burger} ${
-              isActive ? styles.burgerActive : ""
-            } `}
+            className={styles.iconContainer}
           >
+            {isActive ? (
+              <FaTimes className={styles.icon} /> // Close icon
+            ) : (
+              <FaBars className={styles.icon} /> // Hamburger icon
+            )}
             <div ref={ref} className={styles.bounds}></div>
           </div>
         </Magnetic>
